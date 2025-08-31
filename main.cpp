a92771573ddb71d8cde248f643596959d5f6e7e5
Abayjeet Singh <ajstsubasa@gmail.com>
2025-08-31 15:23:17 -0700

    Initial Commit for N-Body problem

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <raylib.h>
+#include"point.h"
+#include <vector>
+#include <ctime>    // for time()
+
+using namespace std;
+
+int main () {
+
+    const int SCREEN_WIDTH = 1800;
+    const int SCREEN_HEIGHT = 1200;
+
+
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "N-Body Problem");
+    SetTargetFPS(60);
+
+    std::srand(std::time(nullptr)); // Seed random generator
+
+    // Create 1000 bodies
+    std::vector<Body> bodies(1000);
+
+    for (size_t i = 0; i < bodies.size(); i++) {
+        bodies[i].radius = 5;              // radius 2..11
+        bodies[i].x = bodies[i].radius + std::rand() % (SCREEN_WIDTH - (int)bodies[i].radius*2);
+        bodies[i].y = bodies[i].radius + std::rand() % (SCREEN_HEIGHT - (int)bodies[i].radius*2);
+    }
+
+    while (WindowShouldClose() == false){
+   
+    
+        
+        BeginDrawing();
+            ClearBackground(BLACK);
+            for (const Body &b : bodies) {
+            DrawCircle((int)b.x, (int)b.y, b.radius, WHITE);
+        }
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
---
+++ point.h
@@ -0,0 +1,20 @@
+#include<cstdint>
+struct Body {
+    public:
+        double x;
+        double y;
+        double radius ;
+
+    Body(double x , double y , double radius){
+
+        this->x = x;
+        this->y = y;
+        this->radius = radius;
+
+    }
+    Body(){
+        
+    }
+    
+
+};
